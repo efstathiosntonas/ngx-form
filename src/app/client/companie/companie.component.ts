c6337b30f3925f42bc2babb4b06ba3b6702d79e0
Alan <alan345@gmail.com>
2016-12-14 16:05:21 -0800

    no modal stable

--- src/app/client/companie/companie.component.ts
+++ src/app/client/companie/companie.component.ts
@@ -2,9 +2,6 @@ import {Component, OnInit} from '@angular/core';
 import {AuthService} from '../auth/auth.service';
 import {CompanieService} from './companie.service';
 import {ChangeDetectionStrategy, Input} from "@angular/core";
-import { ViewContainerRef, ViewEncapsulation } from '@angular/core';
-import { Overlay } from 'angular2-modal';
-import { Modal } from 'angular2-modal/plugins/bootstrap';
 
 
 
@@ -26,13 +23,9 @@ export class CompanieComponent implements OnInit {
   };
 
   constructor(
-    private companieService: CompanieService,
-    overlay: Overlay,
-    vcRef: ViewContainerRef,
-    public modal: Modal
+    private companieService: CompanieService
   ) {
     this.getCompanies(this.paginationData.currentPage);
-    overlay.defaultViewContainer = vcRef;
   }
 
   getPage(page: number) {
@@ -62,22 +55,7 @@ export class CompanieComponent implements OnInit {
   }
 
   onEdit() {
-    this.modal.alert()
-        .size('lg')
-        .showClose(true)
-        .title('A simple Alert style modal window')
-        .body(`
-            <h4>Alert is a classic (title/body/footer) 1 button modal window that
-            does not block.</h4>
-            <b>Configuration:</b>
-            <ul>
-                <li>Non blocking (click anywhere outside to dismiss)</li>
-                <li>Size large</li>
-                <li>Dismissed with default keyboard key (ESC)</li>
-                <li>Close wth button click</li>
-                <li>HTML content</li>
-            </ul>`)
-        .open();
+
   }
 
 
