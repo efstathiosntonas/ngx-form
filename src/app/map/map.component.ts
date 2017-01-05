82a31b99ce6e296adaffd1984a8b623851d94371
Alan <alan345@gmail.com>
2017-01-04 16:16:05 -0800

    label marker map

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -8,8 +8,8 @@ import {ChangeDetectionStrategy, Input} from "@angular/core";
 import {NgbModal, NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
 import {ToastsManager} from 'ng2-toastr';
 import { Inject, forwardRef} from '@angular/core';
-
-
+import {AgmCoreModule} from 'angular2-google-maps/core';
+import { MouseEvent } from "angular2-google-maps/core";
 
 
 @Component({
@@ -20,18 +20,73 @@ import { Inject, forwardRef} from '@angular/core';
 })
 export class MapComponent implements OnInit {
 
-    title: string = 'My first angular2-google-maps project';
-    lat: number = 51.678418;
-    lng: number = 7.809007;
+    zoom: number = 8;
+    lat: number = 51.673858;
+    lng: number = 7.815982;
 
-  constructor(
-    private toastr: ToastsManager
-  ) {
+
+    markers: marker[] = [
+  	  {
+  		  lat: 51.673858,
+  		  lng: 7.815982,
+  		  label: 'A',
+  		  draggable: true
+  	  },
+  	  {
+  		  lat: 51.373858,
+  		  lng: 7.215982,
+  		  label: 'B',
+  		  draggable: false
+  	  },
+  	  {
+  		  lat: 51.723858,
+  		  lng: 7.895982,
+  		  label: 'C',
+  		  draggable: true
+  	  }
+    ]
+
+
+    constructor(
+      private toastr: ToastsManager
+    ) {}
+
+    clickedMarker(label: string, index: number) {
+      console.log(`clicked the marker: ${label || index}`)
+    }
+
+
+
+  mapClicked($event: MouseEvent) {
+
+    this.markers.push({
+      lat: $event.coords.lat,
+      lng: $event.coords.lng,
+      label: "New position",
+      draggable: true
+
+    });
+  }
+
+
+
+  markerDragEnd(m: marker, $event: MouseEvent) {
+    let markerSected: marker;
+    markerSected = this.markers.filter(function (marker) { return marker.label == m.label })[0];
+    markerSected.lat = $event.coords.lat;
+    markerSected.lng = $event.coords.lng;
 
   }
 
   ngOnInit() {
-
-
   }
+}
+
+
+// just an interface for type safety.
+interface marker {
+	lat: number;
+	lng: number;
+	label?: string;
+	draggable: boolean;
 }
