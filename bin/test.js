dbdbe3741c7f8cc6d7810e27da5aeea69041db86
Alan <alan345@gmail.com>
2017-03-11 18:59:48 -0800

    test

--- bin/test.js
+++ bin/test.js
@@ -3,4 +3,7 @@ http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello Node.js\n');
 }).listen(8124, "127.0.0.1");
-console.log('Server running at http://127.0.0.1:8124/');
+console.log('Server running at http://52.7.166.211:8124/');
+
+//https://howtonode.org/how-to-install-nodejs
+//https://www.youtube.com/watch?v=N3vgozk66dI
