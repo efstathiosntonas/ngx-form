9b02b3977c2d671d796e93fab19aaa8aac46f60e
Alan <alan345@gmail.com>
2017-03-11 18:50:53 -0800

    test

--- bin/test.js
+++ bin/test.js
@@ -2,5 +2,5 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello Node.js\n');
-}).listen(8124, "52.7.166.211/");
-console.log('Server running at http://http://52.7.166.211/');
+}).listen(8124, "127.0.0.1");
+console.log('Server running at http://127.0.0.1:8124/');
