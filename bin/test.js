553db9f0f6ac7db4d85de3d7d4de1a90680b2cf4
Alan <alan345@gmail.com>
2017-03-11 18:38:23 -0800

    test

--- bin/test.js
+++ bin/test.js
@@ -1,8 +1,9 @@
 var http = require('http');
 var server = http.createServer(function(request, response) {
   response.writeHead(200,{"Content-Type":"text/plain"});
-  response.end('hello');
+  responses.end('hello');
 
 });
 server.listen(3000);
 console.log('server running');
+//https://www.youtube.com/watch?v=N3vgozk66dI
