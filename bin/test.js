9f29a54c8f8065bc5585f065f46ed152ba0ee6fa
Alan <alan345@gmail.com>
2017-03-12 19:57:24 -0700

    test

--- bin/test.js
+++ bin/test.js
@@ -1,15 +1,9 @@
-var express = require('express');
-var app = express();
-
-app.get('/', function(req, res){
-  res.send('Hello World');
-});
-
-var server = app.listen(3000, function(){
-  console.log('Magic is happening on port 3000')
-});
-
-
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello Node.js\n');
+}).listen(8124, "127.0.0.1");
+console.log('Server running at http://52.7.166.211:8124/');
 
 //https://howtonode.org/how-to-install-nodejs
 //https://www.youtube.com/watch?v=N3vgozk66dI
