a3bf89f5965601a4a15f5ddc9919ac9ffc058160
Alan <alan345@gmail.com>
2017-03-17 14:38:40 -0700

    test

--- bin/www.js
+++ bin/www.js
@@ -12,10 +12,6 @@ var http = require('http');
  * Get port from environment and store in Express.
  */
 
-//PROD
-//var port = normalizePort(process.env.PORT || '3000');
-
-//DEV
 var port = normalizePort(process.env.PORT || '80');
 app.set('port', port);
 
