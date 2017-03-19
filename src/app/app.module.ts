5c4e599a8b99b38d4fc5f9e5087321e374efdea5
Alan <alan345@gmail.com>
2017-03-19 15:01:11 -0700

    modal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -35,7 +35,7 @@ import {AdminService} from './admin/services/admin.service';
 import {EditUsersFormsComponent} from './admin/editUsersForms/editUsersForms.component';
 
 import {EditOptionsComponent} from './admin/editOptions/editOptions.component';
-import {EditOptionsComponentDialog} from './admin/editOptions/editOptions.component';
+import {EditOptionsDialogComponent} from './admin/editOptions/editOptionsDialog.component';
 
 import {EditOptionsService} from './admin/editOptions/editOptions.service';
 
@@ -95,7 +95,7 @@ export function authHttpServiceFactory(http: Http, options: RequestOptions) {
     AdminPageComponent,
     EditUsersFormsComponent,
     EditOptionsComponent,
-    EditOptionsComponentDialog,
+    EditOptionsDialogComponent,
     AdminComponent,
     UserProfileComponent,
     ChangePasswordComponent,
